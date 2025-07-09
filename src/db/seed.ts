9927086ab5ad62f1ba591987fa601257885b0bc0
DiottoDev <nicodiottodev@gmail.com>
2025-07-09 17:55:13 -0300

    feat: update database seed with sample data

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,13 +3,16 @@ import { db, sql } from './connection.ts'
 import { schema } from './schemas/index.ts'
 
 await reset(db, schema)
-await seed(db, schema).refine((result) => {
+await seed(db, schema).refine((f) => {
   return {
     rooms: {
       count: 20,
       columns: {
-        name: result.companyName(),
-        description: result.loremIpsum(),
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+      questions: {
+        count: 20,
       },
     },
   }
