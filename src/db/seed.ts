7505274cb53d00ca16b3945c17a2b5e0aebf4833
DiottoDev <nicodiottodev@gmail.com>
2025-07-09 14:01:49 -0300

    feat: add rooms listing

---
+++ src/db/seed.ts
@@ -0,0 +1,18 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schemas/index.ts'
+
+await reset(db, schema)
+await seed(db, schema).refine((result) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: result.companyName(),
+        description: result.loremIpsum(),
+      },
+    },
+  }
+})
+
+await sql.end()
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { log } from 'node:console'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.register(fastifyCors, {
+  origin: '*',
+})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+app.get('/health', () => {
+  return { status: 'OK' }
+})
+app.register(getRoomsRoute)
+app.listen({ port: env.PORT }, (err) => {
+  if (err) {
+    log(err, 'Server failed to start')
+    process.exit(1)
+  }
+  log(`Server is running at http://localhost:${process.env.PORT || 3333}`)
+})
