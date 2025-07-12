5ecd47a7e606ad5e4afb614e1c584ef96d5b004e
DiottoDev <nicodiottodev@gmail.com>
2025-07-12 18:56:04 -0300

    feat: add multipart support and audio upload route to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { log } from 'node:console'
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -11,8 +12,10 @@ import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { getRoomQuestionsRoute } from './http/routes/get-rooms-questions.ts'
+import { uploadRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.register(fastifyMultipart)
 app.register(fastifyCors, {
   origin: '*',
 })
@@ -25,6 +28,7 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestionsRoute)
 app.register(createQuestionRoute)
+app.register(uploadRoute)
 app.listen({ port: env.PORT }, (err) => {
   if (err) {
     log(err, 'Server failed to start')
